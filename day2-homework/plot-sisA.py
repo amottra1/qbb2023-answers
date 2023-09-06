4b965b112bbdbb425e33e8a711967b9bf1ebad77
Alexis <amottra1@jh.edu>
2023-09-06 16:08:45 -0400

    Add answer for exercise 1, step 2

--- day2-homework/plot-sisA.py
+++ day2-homework/plot-sisA.py
@@ -17,6 +17,7 @@ Add 2*male data (HINT: 2 * np.array( y ))
 Annotate plot (generalize x-axis to 10 not female_10, add title, add x- and y-axis labels)"""
 
 #For Commit 1: Plot female data
+#For Commit 2: Add male data
 
 transcripts = np.loadtxt( "all_annotated.csv", delimiter=",", usecols=0, dtype="<U30", skiprows=1 )
 print( "transcripts: ", transcripts[0:5] )
@@ -38,22 +39,33 @@ for i in range(len(samples)):
     if "female" in samples[i]:
         cols.append(i)
 
+#add males as new set of data
+colsm = []
+for im in range(len(samples)):
+    if "female" not in samples[im]:
+        colsm.append(im)
+
 # Subset data of interest
 expression = data[row, cols]
+expressionm = data[row, colsm]
 
 # Prepare data
 x = samples[cols]
 y = expression
+ym =expressionm
 
-# Plot data
+#Plot data
 fig, ax = plt.subplots()
 ax.set_title( "FBtr0073461" )
 ax.plot( x, y )
+ax.plot(x, ym) #add male data
 
 plt.xticks(rotation=90) #rotate x-axis labels
 ax.set_title( "Sxl (FBtr0073461)" ) #rename title
-ax.set_xlabel( "Devlopmental Stage") #set x-axis label
+ax.set_xlabel( "Developmental Stage") #set x-axis label
 ax.set_ylabel("mRNA Abundance (RPKM)") #set y-axis label
+
+
 
 plt.tight_layout()
 fig.savefig( "FBtr0073461.png" )
